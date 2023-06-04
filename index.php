b3129eca4653a93111d20031d5e9f5b40c5c028e
mardiyanto <mardybest@gmail.com>
2023-06-04 20:52:00 +0700

    fix

--- index.php
+++ index.php
@@ -72,8 +72,7 @@ session_start();
 									<div class="banner-content text-center">
 										<h4 class="text-white mb-20 text-uppercase">Discover the Colorful World</h4>
 										<h1 class="text-uppercase text-white">New Adventure</h1>
-										<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp <br>
-										or incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+										<p class="text-white">Berbagi Kebahagian Bersama keluarga bersama kami di sini</p>
 										<?php if( $_SESSION['nama']==''){?>
 											<a href="booking.php?aksi=home" class="text-uppercase header-btn">PESAN TIKET SEKARANG</a>
 <?php }else{?>
@@ -93,8 +92,7 @@ session_start();
 									<div class="banner-content text-center">
 										<h4 class="text-white mb-20 text-uppercase">Discover the Colorful World</h4>
 										<h1 class="text-uppercase text-white">New Trip</h1>
-										<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp <br>
-										or incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+										<p class="text-white">Berbagi Kebahagian Bersama keluarga bersama kami di sini</p>
 										<?php if( $_SESSION['nama']==''){?>
 											<a href="booking.php?aksi=home" class="text-uppercase header-btn">PESAN TIKET SEKARANG</a>
 <?php }else{?>
@@ -113,8 +111,7 @@ session_start();
 									<div class="banner-content text-center">
 										<h4 class="text-white mb-20 text-uppercase">Discover the Colorful World</h4>
 										<h1 class="text-uppercase text-white">New Experience</h1>
-										<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp <br>
-										or incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+										<p class="text-white">Berbagi Kebahagian Bersama keluarga bersama kami di sini</p>
 										<?php if( $_SESSION['nama']==''){?>
 											<a href="booking.php?aksi=home" class="text-uppercase header-btn">PESAN TIKET SEKARANG</a>
 <?php }else{?>
@@ -342,23 +339,9 @@ Lokasi wisata tersebut juga mempunyai kantin dengan menu andalan, ikan bakar, ay
 								<p>UNTUK MENGHUNGI KAMI BISA MELALU LINK DI BAWAH INI</p>
 							</div>
 						</div>
-					</div>										
-					<form class="form-area " id="myForm" action="mail.php" method="post" class="contact-form text-right">
-						<div class="row">	
-						<div class="col-lg-6 form-group">
-							<input name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mb-20 form-control" required="" type="text">
-						
-							<input name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control" required="" type="email">
-
-							<input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
-						</div>
-						<div class="col-lg-6 form-group">
-							<textarea class="common-textarea mt-10 form-control" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required=""></textarea>
-							<button class="primary-btn mt-20">Send Message<span class="lnr lnr-arrow-right"></span></button>
-							<div class="alert-msg">								
-						</div>
-						</div></div>
-					</form>						
+					</div>
+					<p>Jl. Raya Gisting Atas No.13, Gisting Bawah, Kec. Gisting, Kabupaten Tanggamus, Lampung 35378	</p>									
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4804.457323896059!2d104.72267641476535!3d-5.443881696051951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e47231534559909%3A0x455df3792a157569!2sLentana%20Garden!5e1!3m2!1sid!2sid!4v1685886550209!5m2!1sid!2sid" width="700" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>		
 					
 				</div>	
 			</section>
@@ -370,54 +353,13 @@ Lokasi wisata tersebut juga mempunyai kantin dengan menu andalan, ikan bakar, ay
 					<div class="row">
 						<div class="col-lg-5 col-md-6 col-sm-6">
 							<div class="single-footer-widget">
-								<h6>About Us</h6>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
-								</p>
+								
 								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             		<p class="footer-text">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
             		<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 							</div>
 						</div>
-						<div class="col-lg-5  col-md-6 col-sm-6">
-							<div class="single-footer-widget">
-								<h6>Newsletter</h6>
-								<p>Stay update with our latest</p>
-								<div class="" id="mc_embed_signup">
-
-										<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
-
-										<div class="d-flex flex-row">
-
-											<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
-
-
-				                            	<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
-				                            	<div style="position: absolute; left: -5000px;">
-													<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-												</div>
-				                          	
-											<!-- <div class="col-lg-4 col-md-4">
-												<button class="bb-btn btn"><span class="lnr lnr-arrow-right"></span></button>
-											</div>  -->
-										</div>		
-										<div class="info"></div>
-										</form>
-								</div>
-								</div>
-						</div>						
-						<div class="col-lg-2 col-md-6 col-sm-6 social-widget">
-							<div class="single-footer-widget">
-								<h6>Follow Us</h6>
-								<p>Let us be social</p>
-								<div class="footer-social d-flex align-items-center">
-									<a href="#"><i class="fa fa-facebook"></i></a>
-									<a href="#"><i class="fa fa-twitter"></i></a>
-									<a href="#"><i class="fa fa-dribbble"></i></a>
-									<a href="#"><i class="fa fa-behance"></i></a>
-								</div>
-							</div>
-						</div>							
+										
 					</div>
 				</div>
 			</footer>	
